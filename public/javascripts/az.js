5deab5c91383a0a39f9c1338ab748a31ecd78425
az <1457800393@qq.com>
2017-04-12 15:34:39 +0800

    4-12 3:34

--- public/javascripts/az.js
+++ public/javascripts/az.js
@@ -1,13 +1,54 @@
 window.addEventListener('load',function(){
-	$.ajax({
-			type: "get",
+	
+	$('.btn').click(function(){
+		var text1=$('.text1').val();
+		var text2=$('.text2').val();
+		var text3=$('.text3').val();
+		console.log(text1)
+		console.log(text2)
+		console.log(text3)
+//		$.ajax({
+//			type: "get",
+//			url: "http://localhost:9664/item/list",
+//			async: true,
+//			data:{
+//				id:text1,count:text2,price:text3
+//			},
+//			success: function(data) {
+////				console.log(data[0].spending);
+//				console.log(data)
+////				for(var i=0;i<data[0].spending.length;i++){
+////					console.log(data[0].spending[i].time);
+////					console.log(data[0].spending[i].category);
+////					console.log(data[0].spending[i].count);
+////				}
+//				text1='';
+//				text2='';
+//				text3='';
+//			 }
+//		  })
+		
+		$.ajax({
+			type: "post",
 			url: "http://localhost:9664/item/list",
 			async: true,
+			data:{
+				'id':text1,'count':text2,'price':text3
+			},
 			success: function(data) {
-				console.log(data);				
-				
+				console.log(data)
+				text1='';
+				text2='';
+				text3='';
 			 }
 		  })
+		
+		
+		
+		
+		
+	})
+	
 	
 	
 	
--- routes/item.js
+++ routes/item.js
@@ -2,8 +2,40 @@ var express = require('express');          //引入express文件
 var router = express.Router();             // 提取express的router方法
 router.get('/list',function(request,response){    //第一个请求参数，第二个响应参数
 	console.log('it reading...');                 //打印在服务器
-	response.send('congrouteltion');              //打印在控制器
+	var id = request.query.id;
+	var count = request.query.count;
+	var price = request.query.price;
+	console.log(id)
+	console.log(count)
+	console.log(price)
+	response.send(
+		[{spending:[
+            {time:'jan',category:'shoping',count:2000},
+			{time:'feb',category:'shoping',count:3000},
+			{time:'march',category:'cart',count:500},
+			{time:'april',category:'traffic',count:100},
+			{time:'may',category:'cart',count:1000},
+			{time:'june',category:'cart',count:800},
+			{time:'july',category:'shoping',count:2000},
+			{time:'aug',category:'traffic',count:400},
+			{time:'sep',category:'traffic',count:500}	
+]}]			
+	);              
+	//打印在控制器
 })
+
+router.post('/list',function(request,response){ 
+	    console.log('post is ok');     
+       	var id = request.body.id;
+       	var count = request.body.count;
+   		var price = request.body.price;
+        console.log('id'+id+'count'+count+'price'+price);
+})
+
+
+
+
+
 
 
 
